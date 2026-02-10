db586e6d96a16b94eff5fc833876546e2bbdd191
Nicolas Contreras <nicolas.contreras76@inacapmail.cl>
2026-02-10 00:10:45 -0300

    sistema-gestion-tributaria
    termine de hace mi componenete sistema-gestion tributaria

--- landing-page/src/app/pages/sistema-gestion-tributaria/sistema-gestion-tributaria.ts
+++ landing-page/src/app/pages/sistema-gestion-tributaria/sistema-gestion-tributaria.ts
@@ -1,6 +1,7 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-gestion-tributaria',
@@ -9,11 +10,33 @@ import { Router } from '@angular/router';
   templateUrl: './sistema-gestion-tributaria.html',
   styleUrls: ['./sistema-gestion-tributaria.css']
 })
-export class GestionTributariaComponent {
 
-  constructor(private router: Router) {}
+export class GestionTributariaComponent implements OnInit {
+
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngOnInit(): void {
+    // Solo ejecutar en el navegador, no en el servidor
+    if (isPlatformBrowser(this.platformId)) {
+      window.scrollTo(0, 0);
+    }
+  }
 
   goBack(): void {
     this.router.navigate(['/']);
   }
+
+  selectedImage: string | null = null;
+
+  openImage(src: string) {
+    this.selectedImage = src;
+  }
+
+  closeImage() {
+    this.selectedImage = null;
+  }
+  
 }
